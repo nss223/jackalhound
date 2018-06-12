daca204af03fa0426f12952c2f2aeb6090ac17f2
jack@zhou <jiyuanyi1992@gmail.com>
2018-06-12 23:55:44 +0800

    data flow in jclient

--- hx_jclient/src/test/java/org/lab2528/hx_jclient/TestData.java
+++ hx_jclient/src/test/java/org/lab2528/hx_jclient/TestData.java
@@ -4,56 +4,112 @@ import org.lab2528.hx_jclient.UserManagement;
 import org.lab2528.hx_jclient.Util;
 
 public class TestData {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Demo 4
-        // data flow
-    	
-        StringBuilder txid;
-        String res;
+		// Demo 4
+		// data flow
 
-        try {
-            // set user context
-            Util.client.setUserContext(UserManagement.getOrCreateUser(""));
+		StringBuilder txid;
+		String res, user;
 
-            Util.log.info("Register user `user_sp'");
-            txid = new StringBuilder();
-            if (Util.invoke("regchannel", "regcc", "createUser",
-                    new String[] { "user_sp" }, txid))
-                Util.log.info("Success: " + txid);
+		try {			
+			user = "Amy";
+			Util.log.info("Set user context as `" + user + "'");
+			Util.client.setUserContext(UserManagement.getOrCreateUser(user));
+			Util.log.info("Commit data `id0'");
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "commit",
+					new String[] { "id0", "db://id0", "key", "chash", "chash", "doc of id0" }, txid))
+				Util.log.info("Success: " + txid);
+			Util.log.info("Commit data `id1'");
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "commit",
+					new String[] { "id1", "db://id1", "key", "chash", "chash", "doc of id1" }, txid))
+				Util.log.info("Success: " + txid);
+			
+			user = "Bob";
+			Util.log.info("Set user context as `" + user + "'");
+			Util.client.setUserContext(UserManagement.getOrCreateUser(user));
+			Util.log.info("Commit data `id3'");
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "commit",
+					new String[] { "id3", "db://id3", "key", "chash", "chash", "doc of id3" }, txid))
+				Util.log.info("Success: " + txid);
+						
+			Util.log.info("Checkout data");
+			res = Util.query("datachannel", "datacc", "checkout", new String[] { "id3" });
+			Util.log.info(res);
+		} catch (Exception e) {
+			Util.log.error(e.getMessage());
+		}
+		
+		try {
+			Util.log.info("Checkout others' data");
+			res = Util.query("datachannel", "datacc", "checkout", new String[] { "id1" });
+			Util.log.info(res);
+		} catch (Exception e) {
+			Util.log.error(e.getMessage());
+		}
+		
+		try {
+			Util.log.info("Share data without permission");
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "share",
+					new String[] { "id1", "Bob" }, txid))
+				Util.log.info("Success: " + txid);
+		} catch (Exception e) {
+			Util.log.error(e.getMessage());
+		}
+		
+		try {
+			Util.log.info("Share data by the owner");
+			Util.client.setUserContext(UserManagement.getOrCreateUser("Amy"));
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "share",
+					new String[] { "id1", "Bob" }, txid))
+				Util.log.info("Success: " + txid);
+			
+			Util.client.setUserContext(UserManagement.getOrCreateUser("Bob"));
+			Util.log.info("Checkout others' data after sharing");
+			res = Util.query("datachannel", "datacc", "checkout", new String[] { "id1" });
+			Util.log.info(res);
+			
+			Util.log.info("Make a new branch");
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "branch",
+					new String[] { "id1", "id4", "db://id4", "key", "chash", "chash", "doc of id4" }, txid))
+				Util.log.info("Success: " + txid);
+			txid = new StringBuilder();
+			if (Util.invoke("datachannel", "datacc", "branch",
+					new String[] { "id4", "id5", "db://id5", "key", "chash", "chash", "doc of id5" }, txid))
+				Util.log.info("Success: " + txid);
+		} catch (Exception e) {
+			Util.log.error(e.getMessage());
+		}
+		
+		try {			
+			Util.log.info("Check the trace of `id5'");
+						res = Util.query("datachannel", "datacc", "trace", new String[] { "id5" });
+			Util.log.info(res);
+			
+			Util.log.info("Check the history of `id1'");
+			res = Util.query("datachannel", "datacc", "history", new String[] { "id1" });
+			Util.log.info(res);
 
-            // set user context
-            Util.client.setUserContext(UserManagement.getOrCreateUser("user_sp"));
+			Util.log.info("Collect data by owner `Bob'");
+			res = Util.query("datachannel", "datacc", "queryByOwner", new String[] { });
+			Util.log.info(res);
 
-            Util.log.info("Create user `user_sp'");
-            txid = new StringBuilder();
-            if (Util.invoke("mapchannel", "mapcc", "createUser", new String[] { "user_sp" }, txid))
-                Util.log.info("Success: " + txid);
-
-            Util.log.info("Create AccountCL for the user `user_sp'");
-            txid = new StringBuilder();
-            if (Util.invoke("mapchannel", "mapcc", "createAccountCL",
-                    new String[] { "user_sp", "Carol_Credit_0", "Credit", "zyp", "Nothing", "300", "zyp" }, txid))
-                Util.log.info("Success: " + txid);
-
-            Util.log.info("Split some of asset to sub-account");
-            txid = new StringBuilder();
-            if (Util.invoke("mapchannel", "mapcc", "splitAccountCL",
-                    new String[] { "Carol_Credit_0", "user_sp", "Alice_Credit_1", "200" }, txid))
-                Util.log.info("Success: " + txid);
-
-            Util.log.info("Query parent account");
-            res = Util.query("mapchannel", "mapcc", "queryAccount", new String[] { "Carol_Credit_0", "CL" });
-            Util.log.info(res);
-
-            Util.log.info("Query sub-account");
-            res = Util.query("mapchannel", "mapcc", "queryAccount", new String[] { "Alice_Credit_1", "CL" });
-            Util.log.info(res);
-        } catch (Exception e) {
-            Util.log.error(e.getMessage());
-        }
-
-        Util.log.info("End.");
-        return;
+			Util.log.info("Collect data by create `Amy'");
+			Util.client.setUserContext(UserManagement.getOrCreateUser("Amy"));
+			res = Util.query("datachannel", "datacc", "queryByCreater", new String[] { });
+			Util.log.info(res);
+			
+		} catch (Exception e) {
+			Util.log.error(e.getMessage());
+		}
+		
+		Util.log.info("End.");
+		return;
 	}
 }
